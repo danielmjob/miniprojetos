578405b7ba6632fd64dbab9977e6a19c075bec20
danielmjob <danielmarquesjob@gmail.com>
2024-07-26 12:00:02 -0300

    consertando bug da lampada quebrada

--- 01lampada/lampada.js
+++ 01lampada/lampada.js
@@ -2,20 +2,25 @@ const turnOn = document.getElementById ('turnOn');
 const turnOff = document.getElementById('turnOff');
 const lamp =  document.getElementById ('lamp');
 
-function lampOn(){
-    lamp.src = './img/ligada.jpg';
+function isLampBronken(){
+    return lamp.src.indexOf('quebrada') > -1 // procura se a imagem é da lampada quebrada
+}
 
+function lampOn(){
+    if ( !isLampBronken() ){
+        lamp.src = './img/ligada.jpg';
+    }
 }
 
 function lampOff(){
-    lamp.src = './img/desligada.jpg';
+    if (!isLampBronken()){
+        lamp.src = './img/desligada.jpg';
+    }
 }
 
 function lampBroken(){
     lamp.src = './img/quebrada.jpg';
 }
-
-
 
 turnOn.addEventListener ('click',lampOn);
 turnOff.addEventListener ('click', lampOff)
